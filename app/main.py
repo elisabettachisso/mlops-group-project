ae8aa6a68fc23afd9c576351f45060e933da2ace
huseinovicelma <huseinovicelma@gmail.com>
2024-12-14 15:25:14 +0100

    check modified

--- app/main.py
+++ app/main.py
@@ -23,12 +23,13 @@ def main():
     if st.session_state.logged_in == True:
         st.session_state.page = "main"
 
-    if st.session_state.logged_in and st.session_state.username == "dataanalyst" and st.session_state.page == "main":
-        main_page_analyst()
-    elif st.session_state.logged_in and st.session_state.username == "admin" and st.session_state.page == "main":
-        main_page_admin()
-    elif st.session_state.logged_in and st.session_state.page == "main":
-        main_page_users()
+    if st.session_state.logged_in and st.session_state.page == "main":
+        if st.session_state.username == "dataanalyst":
+            main_page_analyst()
+        elif st.session_state.username == "admin":
+            main_page_admin()
+        else:
+            main_page_users()
     elif st.session_state.page == "home":
         home_page()
     elif st.session_state.page == "login":
